064c6d835119ab09c7b30a1aa8d19b3228aaa93c
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-07-17 21:52:40 +0500

    backgroound color changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-neutral-800 h-screen">
       {/* <h1 className="text-3xl">My Personal Portfolio</h1> */}
 
       <Haider />
