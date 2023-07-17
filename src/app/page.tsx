5c1ddc5464c2b413be9acdda5cfe44b37ed9142f
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-07-17 15:16:28 +0500

    social icons added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Haider from "@/components/Haider";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl">My Personal Portfolio</h1>
+      {/* <h1 className="text-3xl">My Personal Portfolio</h1> */}
 
       {/* Header */}
       <Haider />
--- src/components/Haider.tsx
+++ src/components/Haider.tsx
@@ -1,11 +1,47 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 const Haider = (props: Props) => {
   return (
     <header>
-      <h1>I am header</h1>
+      <nav>
+        {/* Social Icons */}
+        <div className="flex">
+          <SocialIcon
+            bgColor="transparent"
+            fgColor="gray"
+            url="https://www.youtube.com/@JawwadAhmadDev"
+          />
+          <SocialIcon
+            fgColor="gray"
+            bgColor="transparent"
+            url="https://www.linkedin.com/in/jawwad-ahmad-2b2334246/"
+          />
+          <SocialIcon
+            fgColor="gray"
+            bgColor="transparent"
+            url="https://github.com/JawwadAhmadDev"
+          />
+          {/* <SocialIcon
+            fgColor="gray"
+            bgColor="transparent"
+            url="https://www.instagram.com/haiderkhan/"
+          /> */}
+          {/* <SocialIcon
+            fgColor="gray"
+            bgColor="transparent"
+            url="https://www.facebook.com/haiderkhan"
+          /> */}
+        </div>
+        {/* Email Link */}
+        <SocialIcon
+          fgColor="gray"
+          bgColor="transparent"
+          url="mailto:<jawwadahmad.edu@gmail.com>"
+        />
+      </nav>
     </header>
   );
 };
