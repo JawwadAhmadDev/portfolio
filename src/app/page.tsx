2b217ca6cca7bf32901943d32704c1016e113f41
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-07-17 19:46:27 +0500

    hero section created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import Haider from "@/components/Haider";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
       {/* <h1 className="text-3xl">My Personal Portfolio</h1> */}
 
-      {/* Header */}
       <Haider />
 
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
 
       {/* About */}
 
