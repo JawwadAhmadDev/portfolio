da5ddeb880b76074b65050a387b29203861fb258
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-07-28 22:38:25 +0500

    added home button at the botom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
+import Link from "next/link";
+import { HomeIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
@@ -36,6 +38,14 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer flex justify-center items-center">
+          <div>
+            <HomeIcon className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0" />
+          </div>
+        </footer>
+      </Link>
     </main>
   );
 }
